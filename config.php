2ac70c1f2e8813d9b1192bd800694bf972938ca2
memek123456 <46232817+memek123456@users.noreply.github.com>
2018-12-29 08:53:08 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,44 @@
+<?php
+//$ratio example:
+//$ratio='480*800';
+// on capture data you can get it on "ratio: here"
+
+$ratio='720*1344';
+
+//$tiket example:
+//$tiket='sHp2r312otmCpnGns3mCqJhovNiBh4DPxxxxxxxxxxx';
+//on capture data you can get it on "ticket: here"
+
+$tiket='sHp2r312otmCpnGns3mCqJhovNiBh4jMrtCFbbTOh8-yjXqyiYa7zIS2fWXAeHljho2vl36HkM-64Jtos8t7zK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ';
+
+//$imei example:
+//$imei='9181cxxxxxxxxxx';
+//on capture data you can get it on "meid: here"
+
+$imei='40d0c3d626426c94';
+
+//$brand example:
+//$brand='samsung';
+// on capture data you can get it on "brand: here"
+
+$brand='xiaomi';
+
+
+//$lang example:
+//$lang='en';
+// on capture data you can get it on "language: here"
+
+$lang='in';
+
+
+//$version example:
+//$version='2.3.1';
+// on capture data you can get it on "version: here";
+
+$version='2.3.1';
+
+
+
+
+
+?>
